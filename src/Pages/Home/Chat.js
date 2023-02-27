1127cb3901800036da8e82b5b34cba9c05c4cef5
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2023-02-27 20:13:15 +0530

    chat app implemented

--- src/Pages/Home/Chat.js
+++ src/Pages/Home/Chat.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Chat(){
-    return<div>
+    return<div >
         Chat
     </div>
 }
--- src/index.js
+++ src/index.js
@@ -7,16 +7,14 @@ import {RoomsProvider} from "./context/room.context";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<BrowserRouter>
-    <ProfileProvider>
-        <RoomsProvider>
-            <App />
-        </RoomsProvider>
+    <BrowserRouter>
+        <ProfileProvider>
+            <RoomsProvider>
+                <App/>
+            </RoomsProvider>
 
-    </ProfileProvider>
+        </ProfileProvider>
 
-</BrowserRouter>
-
-
+    </BrowserRouter>
 );
 
