9cd88ed1ef66072ca3be7a7405922cb086c11271
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2023-02-28 20:46:13 +0530

    chat app implemented

--- src/Pages/Home/Chat.js
+++ src/Pages/Home/Chat.js
@@ -1,9 +1,40 @@
 import React from "react";
+import Top from "../../components/ChatWindow/top/Top";
+import Messages from "../../components/ChatWindow/messages/Messages";
+import Bottom from "../../components/ChatWindow/bottom/Bottom";
+import {useParams} from "react-router";
+import {useRooms} from "../../context/room.context";
+import {Loader} from "rsuite";
 
-export default function Chat(  ){
-    return<div >
-        Chat
-        Chat
+export default function Chat() {
 
-    </div>
+    const {chatId} = useParams();
+    const rooms = useRooms()
+
+    if (!rooms){
+        return <Loader size={"md"} center vertical speed={"slow"} content={"Loading"}/>
+    }
+
+
+    const currentRoom= rooms.find(room=>room.id===chatId)
+
+    if (currentRoom){
+        return <h6 className={"text-center mt-page"}>
+            Chat {chatId} not found
+        </h6>
+    }
+    return <>
+        <div className={"chat-top"}>
+            <Top/>
+        </div>
+        <div className={"chat-middle"}>
+            <Messages/>
+        </div>
+
+        <div className={"chat-bottom"}>
+            <Bottom/>
+        </div>
+
+
+    </>
 }
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -7,20 +7,19 @@ import {useMediaQuery} from "../../helpers/custom-hooks";
 
 
 export default function Index() {
-    // const isDesktop = useMediaQuery('(min-width: 992px)')
-    // const isExact= useMatch('/chat/:chatId');
+    const isDesktop = useMediaQuery('(min-width: 992px)')
+    const {isExact} = useMatch('/chat/:chatId');
 
-    // const canRenderSidebar = isDesktop
-        // || isExact;
+    const canRenderSidebar = isDesktop || isExact;
 
     return <Grid fluid className={"h-100"}>
         <Row className={"h-100"}>
-            {/*{*/}
-            {/*    canRenderSidebar &&*/}
+            {
+                canRenderSidebar &&
                 <Col xs={24} md={8} className={"h-100"}>
                     <Sidebar/>
                 </Col>
-             {/*}*/}
+            }
 
             <Routes>
                 <Route exact path={"/chat/:chatId"} element={
@@ -28,16 +27,21 @@ export default function Index() {
                         <Chat/>
                     </Col>
                 }/>
-                {/*<Route>*/}
-                {/*    {*/}
-                {/*        isDesktop && <Col xs={24} md={16} className={"h-100"}>*/}
-                {/*            <h6 className={"text-center mt-page"}>*/}
-                {/*                Please Select Chat*/}
-                {/*            </h6>*/}
-                {/*        </Col>*/}
 
-                {/*    }*/}
-                {/*</Route>*/}
+
+                <Route  element={
+                    isDesktop &&
+                    <Col xs={24} md={16} className={"h-100"}>
+                        <h6 className={"text-center mt-page"}>
+                            Please Select Chat
+                        </h6>
+                    </Col>
+                }
+
+
+                />
+
+
             </Routes>
 
         </Row>
